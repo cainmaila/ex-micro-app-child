a0b3007e975ba4dfcacb992c0458192b0567a178
cain.chu <cainmaila>
2023-08-22 23:22:01 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ex-micro-app-child/',
+  base: './ex-micro-app-child/',
   build: {
     rollupOptions: {
       output: {
