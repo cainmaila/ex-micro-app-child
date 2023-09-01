4f1d73ea66162318fa97df9fb17309165a578555
cain.chu <cainmaila>
2023-09-01 11:54:16 +0800

    event test

--- view2/View.tsx
+++ view2/View.tsx
@@ -1,3 +1,35 @@
+import { useCallback } from 'react'
+
 export default function View() {
-  return <div>View 2</div>
+  const composedOff = useCallback(() => {
+    dispatchEvent(
+      new CustomEvent('testEvent', {
+        bubbles: true,
+        composed: true,
+        detail: { name: 'composed-Off' },
+      }),
+    )
+  }, [])
+
+  const composedOn = useCallback(() => {
+    dispatchEvent(
+      new CustomEvent('testEvent', {
+        bubbles: true,
+        composed: true,
+        detail: { name: 'composed-On' },
+      }),
+    )
+  }, [])
+
+  return (
+    <div>
+      <h2>事件穿透實驗</h2>
+      <div>
+        <button onClick={composedOff}>不穿透</button>
+      </div>
+      <div>
+        <button onClick={composedOn}>穿透</button>
+      </div>
+    </div>
+  )
 }
